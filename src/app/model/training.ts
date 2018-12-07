eeac2050b215b5efc012a8a39c46bef9c88a130d
sascha iseli <sascha.iseli@gmx.ch>
2018-12-07 13:56:47 +0100

    add details for single training and install ngx-charts

--- src/app/model/training.ts
+++ src/app/model/training.ts
@@ -4,13 +4,13 @@ import { Trackpoint } from './trackpoint';
 export class Training {
 
   constructor(public id: number,
-    public geoJSON: string,
     public dateOfImport: Date,
     public fileName: string,
     public geoQuality: number,
     public note: string,
     public upMeter: number,
     public downMeter: number,
+    public geoJSON: string,
     public trackPoints: Trackpoint[],
     public laps: Lap[]
     ) {}
