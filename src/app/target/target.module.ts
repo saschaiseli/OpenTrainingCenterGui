071570f2164e7586820a8a3d66e77e59ad5a2dd9
sascha iseli <sascha.iseli@gmx.ch>
2018-12-19 23:03:48 +0100

    add modal dialog for training goals

--- src/app/target/target.module.ts
+++ src/app/target/target.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { TargetRoutingModule } from './target-routing.module';
 import { TargetListComponent } from './target-list/target-list.component';
 import { TargetItemComponent } from './target-item/target-item.component';
-import { TargetItemEditModalComponent } from './target-item-edit-modal/target-item-edit-modal.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TargetItemModalComponent } from './target-item-modal/target-item-modal.component';
 
 
 @NgModule({
@@ -17,6 +17,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     TargetRoutingModule
   ],
-  declarations: [TargetListComponent, TargetItemComponent, TargetItemEditModalComponent]
+  declarations: [TargetListComponent, TargetItemComponent, TargetItemModalComponent]
 })
 export class TargetModule { }
